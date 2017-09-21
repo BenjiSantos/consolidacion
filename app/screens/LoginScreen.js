8186dffe17d22de766bbff5f20ab6c9ff495ca9b
BenjiSantos <santsben@gmail.com>
2017-09-21 04:29:05 -0500

    Add effect splash to init the app

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -3,8 +3,12 @@ import React, { Component } from 'react'
 import { Container } from '../components/Container'
 import { Logo } from '../components/Logo'
 import { CardWithActions } from '../components/Card'
+import SplashScreen from 'react-native-splash-screen'
 
 class LoginScreen extends Component {
+    componentDidMount() {
+        SplashScreen.hide();
+    }
     render() {
         return (
             <Container>
